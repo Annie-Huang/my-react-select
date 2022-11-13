6b35d44fec8350f133e5c3459b495c5085d04951
Annie Huang <annie.hn@gmail.com>
2022-11-13 17:32:28 +1100

    Styled the container

--- src/Select.tsx
+++ src/Select.tsx
@@ -13,7 +13,7 @@ type SelectProps = {
 
 const Select = ({ value, onChange, options }: SelectProps) => {
   return (
-    <div className={styles.container}>
+    <div tabIndex={0} className={styles.container}>
       <span className={styles.value}>Value</span>
       <button className={styles['clear-btn']}>&times;</button>
       <div className={styles.divider}></div>
